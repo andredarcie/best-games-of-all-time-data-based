0809b3cfb095c0f45f909104ddc920ea2d51400b
André N. Darcie <andre_darcie@hotmail.com>
2015-10-10 14:29:13 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+    $(".box").mouseenter(function () {
+        $(this).animate({ height: '+=100px' })
+        .children("#tags").find(".tag").show();
+
+    })
+    .mouseleave(function () {
+        $(this).animate({ height: '-=100px' })
+        .children("#tags").find(".tag").hide();
+    });
+
+});
