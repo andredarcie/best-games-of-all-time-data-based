00c1c94be8373c888c19c78fc3542c433a904f99
André N. Darcie <andre_darcie@hotmail.com>
2016-06-21 03:18:36 -0300

    Adding games in the seventh generation

--- json/games.js
+++ json/games.js
@@ -156,6 +156,26 @@ var games = [
     gameList: [
       {
         releaseYear: "2007",
+        title: "BioShock",
+        imageUrl: "http://images.br.sftcdn.net/br/scrn/64000/64999/bioshock-13.jpg",
+        description: "BioShock is set in 1960. The player guides the protagonist, Jack, after his airplane crashes in the ocean near the bathysphere terminus that leads to the underwater city of Rapture.",
+        developer: "2K Boston",
+        platform: "PC, PlayStation 3, Xbox 360",
+        genre: "First-person shooter",
+        videoUrl: "https://www.youtube.com/embed/h7XyG_ktQ5o"
+      },
+      {
+        releaseYear: "2007",
+        title: "Super Mario Galaxy",
+        imageUrl: "http://www.jogoscompletostorrent.com/wp-content/uploads/2015/10/Super-Mario-Galaxy-Wii-2.jpg",
+        description: "The story revolves around the protagonist, Mario, on a quest to rescue Princess Peach and save the universe from Bowser.",
+        developer: "Nintendo EAD Tokyo",
+        platform: "Wii",
+        genre: "Platform",
+        videoUrl: "https://www.youtube.com/embed/_8eJC4gIAm4"
+      },
+      {
+        releaseYear: "2007",
         title: "Call of Duty 4: Modern Warfare",
         imageUrl: "http://1.bp.blogspot.com/-ovJeuPKQjFY/T9PD0WTSaJI/AAAAAAAAHEs/39_8uv0ozGI/s1600/Call+of+duty+4+Modern+Warfare+PC1.jpg",
         description: "The story takes place in the year 2011, where a radical leader has executed the president of an unnamed country in the Middle East, and an ultranationalist movement starts a civil war in Russia.",
@@ -163,7 +183,147 @@ var games = [
         platform: "PC, PlayStation 3, Xbox 360",
         genre: "First-person shooter",
         videoUrl: "https://www.youtube.com/embed/_gBw7ch5IUc"
-      }
+      },
+      {
+        releaseYear: "2007",
+        title: "Portal",
+        imageUrl: "http://vignette2.wikia.nocookie.net/half-life/images/7/74/Testchmb17.jpg/revision/latest?cb=20100310135327&path-prefix=en",
+        description: "The game consists primarily of a series of puzzles that must be solved by teleporting the player's character and simple objects using 'the Aperture Science Handheld Portal Device', a device that can create inter-spatial portals between two flat planes.",
+        developer: "Valve Corporation",
+        platform: "PC, Xbox 360",
+        genre: "Puzzle platformer",
+        videoUrl: "https://www.youtube.com/embed/uWj781LiMtc"
+      },
+      {
+        releaseYear: "2008",
+        title: "Fallout 3",
+        imageUrl: "https://bbot.org/blog-images/fallout3-2-moved.jpg",
+        description: "Fallout 3 takes place in the year 2277, 116 years after the setting of Fallout, 36 years after the setting of Fallout 2 and 200 years after the nuclear apocalypse that devastated the game's world.",
+        developer: "Bethesda Game Studios",
+        platform: "PC, PlayStation 3, Xbox 360",
+        genre: "Action role-playing",
+        videoUrl: "https://www.youtube.com/embed/UC7mP73Jh7s"
+      },
+      {
+        releaseYear: "2007",
+        title: "Grand Theft Auto IV",
+        imageUrl: "http://oceanofgames.com/wp-content/uploads/2014/01/Grand-Theft-Auto-IV-Complete-Edition-Free-Download.jpg",
+        description: "The game is played from a third-person perspective and its world is navigated on-foot or by vehicle. Throughout the single-player mode, players play as Niko Bellic.",
+        developer: "Rockstar North",
+        platform: "PC, PlayStation 3, Xbox 360",
+        genre: "Third-person shooter, action-adventure",
+        videoUrl: "https://www.youtube.com/embed/_HBLXkPLup0"
+      },
+      {
+        releaseYear: "2009",
+        title: "Uncharted 2: Among Thieves",
+        imageUrl: "http://www.mobygames.com/images/shots/l/505116-uncharted-2-among-thieves-playstation-3-screenshot-enemy-soldiers.jpg",
+        description: "The story follows Nathan Drake, as he travels around the world, along with Chloe Frazer and old friends Elena Fisher and Victor Sullivan to find the fabled Cintimani Stone and Shangri-La in a race against Serbian mercenaries.",
+        developer: "Naughty Dog",
+        platform: "PlayStation 3, Xbox 360",
+        genre: "Third-person shooter, action-adventure",
+        videoUrl: "https://www.youtube.com/embed/D_p58o6RJ88"
+      },
+      {
+        releaseYear: "2010",
+        title: "Mass Effect 2",
+        imageUrl: "https://i.ytimg.com/vi/dflOYJ0SGY4/maxresdefault.jpg",
+        description: "The game takes place within the Milky Way galaxy during the 22nd century, where humanity is threatened by an insectoid species known as the Collectors.",
+        developer: "BioWare",
+        platform: "PC, PlayStation 3, Xbox 360",
+        genre: "Action role-playing, third-person shooter",
+        videoUrl: "https://www.youtube.com/embed/FuLFyqzh17M"
+      },
+      {
+        releaseYear: "2010",
+        title: "Super Mario Galaxy 2",
+        imageUrl: "http://gamessaga.com/wp-content/uploads/2014/08/Super-Mario-Galaxy-2-game.jpg",
+        description: "The story follows Mario as he pursues the Koopa King, Bowser, into outer space, where he has imprisoned Princess Peach and taken control of the universe using Power Stars.",
+        developer: "Nintendo EAD Tokyo",
+        platform: "Wii, Wii U",
+        genre: "Platforming",
+        videoUrl: "https://www.youtube.com/embed/eDX_fY3wDM0"
+      },
+      {
+        releaseYear: "2010",
+        title: "Red Dead Redemption",
+        imageUrl: "http://images-cdn.moviepilot.com/images/c_scale,h_675,w_1200/t_mp_quality/k5rmlrnf7r2zijnv3jrm/red-dead-redemption-2-looks-like-this-plot-leak-wasn-t-from-rockstar-red-dead-redemptio-469426.jpg",
+        description: "The game, set during the decline of the American Frontier in the year 1911, follows John Marston, a former outlaw whose wife and son are taken hostage by the government.",
+        developer: "Rockstar San Diego",
+        platform: "PlayStation 3, Xbox 360",
+        genre: "Action-adventure",
+        videoUrl: "https://www.youtube.com/embed/Q8gjm07yqpc"
+      },
+      {
+        releaseYear: "2011",
+        title: "Portal 2",
+        imageUrl: "http://pcmedia.ign.com/pc/image/article/116/1162215/portal-2-20110415070040131-000.jpg",
+        description: "The player controls protagonist Chell, awoken from suspended animation after many years, who must navigate the now-dilapidated Aperture Science Enrichment Center during its reconstruction by the reactivated GLaDOS, a powerful supercomputer.",
+        developer: "Valve Corporation",
+        platform: "PC, PlayStation 3, Xbox 360",
+        genre: "Puzzle-platform game",
+        videoUrl: "https://www.youtube.com/embed/xtBls2xeTFM"
+      },
+      {
+        releaseYear: "2011",
+        title: "Dark Souls",
+        imageUrl: "https://i.ytimg.com/vi/-z7n61MN6YU/maxresdefault.jpg",
+        description: "Dark Souls takes place in the fictional kingdom of Lordran. Players assume the role of a cursed undead character who begins a pilgrimage out of the Undead Asylum to discover the fate of the Undead.",
+        developer: "FromSoftware",
+        platform: "PC, PlayStation 3, Xbox 360",
+        genre: "Action role-playing",
+        videoUrl: "https://www.youtube.com/embed/hnn5QJljg94"
+      },
+      {
+        releaseYear: "2011",
+        title: "The Elder Scrolls V: Skyrim",
+        imageUrl: "http://i11b.3djuegos.com/juegos/5029/the_elder_scrolls_v/fotos/set/the_elder_scrolls_v-1840230.jpg",
+        description: "Skyrim's main story revolves around the player character and their effort to defeat Alduin the World-Eater, a dragon who is prophesied to destroy the world.",
+        developer: "Bethesda Game Studios",
+        platform: "PC, PlayStation 3, Xbox 360",
+        genre: "Action role-playing",
+        videoUrl: "https://www.youtube.com/embed/ed2IfyPNkdw"
+      },
+      {
+        releaseYear: "2011",
+        title: "Batman: Arkham City",
+        imageUrl: "https://lifeculturegeekstuff.files.wordpress.com/2011/03/arkham-city1.jpg",
+        description: "In the game's main storyline, Batman is incarcerated in Arkham City, a huge new super-prison enclosing the decaying urban slums of fictional Gotham City.",
+        developer: "Rocksteady Studios",
+        platform: "PC, PlayStation 3, Xbox 360",
+        genre: "Action-adventure",
+        videoUrl: "https://www.youtube.com/embed/jS2daMdVzAg"
+      },
+      {
+        releaseYear: "2012",
+        title: "Journey",
+        imageUrl: "http://static1.gamespot.com/uploads/screen_kubrick/mig/5/1/8/0/2115180-169_journey_gp_01_030112.jpg",
+        description: "The player controls a robed figure in a vast desert, traveling towards a mountain in the distance.",
+        developer: "Thatgamecompany",
+        platform: "PlayStation 3",
+        genre: "Adventure, art game",
+        videoUrl: "https://www.youtube.com/embed/bkL94nKSd2M"
+      },
+      {
+        releaseYear: "2013",
+        title: "The Last of Us",
+        imageUrl: "https://i.ytimg.com/vi/zbNmmK9Bhzw/maxresdefault.jpg",
+        description: "Players control Joel, a man tasked with escorting a young girl named Ellie across a post-apocalyptic United States.",
+        developer: "Naughty Dog",
+        platform: "PlayStation 3",
+        genre: "Action-adventure, survival horror",
+        videoUrl: "https://www.youtube.com/embed/aUZ3MKvUjx8"
+      },
+      {
+        releaseYear: "2013",
+        title: "Grand Theft Auto V",
+        imageUrl: "http://3.bp.blogspot.com/-sEbzOGIGV8Y/UpKCtQlyWWI/AAAAAAAAAOE/xd0jtE1i6fk/s1600/gta+v.png",
+        description: "Set within the fictional state of San Andreas, based on Southern California, the single-player story follows three criminals and their efforts to commit heists while under pressure from a government agency.",
+        developer: "Rockstar North",
+        platform: "PC, PlayStation 3, Xbox 360",
+        genre: "First-person shooter",
+        videoUrl: "https://www.youtube.com/embed/2H9wiUqJiws"
+      },
     ]
   },
   {
